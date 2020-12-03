e817a9121dbea07c0ce5dd9944a2adbdf56c90ce
Chevon Phillip <chevon@chevonphillip.com>
2020-12-03 16:21:42 -0500

    Fixed allowed host

--- todoApp/settings.py
+++ todoApp/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'o3^9w(h5m8uubeo*(#(xam&za*2o3juqgv47inkm-5qtapumvo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
